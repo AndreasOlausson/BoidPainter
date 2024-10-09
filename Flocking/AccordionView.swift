845071d91bf22328afab868cfd7a1274bcdeadf4
Andreas Olausson <andreas@codepoets.se>
2024-10-09 15:08:04 +0200

    alpha2

---
+++ Flocking/AccordionView.swift
@@ -0,0 +1,18 @@
+//
+//  AccordionView.swift
+//  Flocking
+//
+//  Created by Andreas Olausson on 2024-10-09.
+//
+
+import SwiftUI
+
+struct AccordionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccordionView()
+}
